e6ecf9ee5ee73adeeeaad385378d8c0132a67fdd
Anthony <sean.a.protho1@gmail.com>
2019-08-30 19:17:30 -0700

    Completely Incomplete Update

--- checklist.py
+++ checklist.py
@@ -1,3 +1,4 @@
+from termcolor import colored
 import os
 import time
 
@@ -15,6 +16,8 @@ def read(index):
 
 def update(index, item):
     # Update code here
+    index = colored('green')
+    item = colored('green')
     checklist[index] = item 
 
 def destroy(index):
@@ -30,35 +33,64 @@ def list_all_items():
 
 def mark_completed(index):
     #Adds marked items as complete
-    index = 0
-    for list_item in checklist:
-        print("√{} {}".format(index, list_item))
-        index += 1
+    index = colored('blue')
+    print(f"√{index}")
+
+def mark_incomplete(index):
+    #Adds marks items incomplete
+    print(f"{index}")
 
 def select(function_code):
     #create item
-    if function_code == "C" | function_code == "c":        
+    if function_code == "C" or function_code == "c":        
         item_index = user_input("Index Item: ")
         time.sleep(1)
         os.system("clear")
         create(user_input)
+    #Updates item
+    if function_code == "U" or function_code == "u":
+        item_index = user_input("Index Item: ")
+        new_item = user_input("New Item: ")
+        time.sleep(1)
+        os.system("clear")
+        update(item_index, new_item)
+    #Destroy items
+    elif function_code == "D" or function_code == "d":
+        item_index = user_input("Index Number? ")
+        time.sleep(1)
+        os.system("clear")
+        checklist.pop(item_index)
     # Read item
-    elif function_code == "R" | function_code == "r":
+    elif function_code == "R" or function_code == "r":
         item_index = user_input("Index Number? ")
         #Remebering item_index must exist
         time.sleep(1)
         os.system("clear")
         read(item_index)
     #Prints items
-    elif function_code == "P" | function_code == "p":
+    elif function_code == "P" or function_code == "p":
         time.sleep(1)
         os.system("clear")
         list_all_items()
-    #Exits program
-    elif function_code == "Q" | function_code == "q":
+    #Marks Complete
+    elif function_code == "X" or function_code == "x":
+        item_index = user_input("Index Number? ")
         time.sleep(1)
         os.system("clear")
-        print("Exiting program")
+        mark_completed(item_index)
+    #Marks Incomplete
+    elif function_code == "Z" or function_code == "z":
+        item_index = user_input("Index Number? ")
+        time.sleep(1)
+        os.system("clear")
+        mark_incomplete(item_index)
+    #Exits program
+    elif function_code == "Q" or function_code == "q":
+        _ = "Exiting Program"
+        _ = colored('red')
+        time.sleep(1)
+        os.system("clear")
+        print(_)
         running = False
     #catch all
     else:
@@ -84,24 +116,20 @@ def test():
     list_all_items()
 
 #runs test
-test()
+#test()
 
 running = True
 while running:
     selection = user_input(
-        "Press C to add list, R to read list, and P to display list. Or Q to quite.")
+        "Press C to add list, U to update list, D to remove list, X to mark done, and Z to mark incomplete. You can also use R to read, P to display list or Q to quite. ")
     select(selection)
 
 #ToDO!
-#Add Read, update and destroy options to select func 
 #Allow For user to use upper and lower case commands √
 #Handle errors cuased by invalid user inputs √
 
 #Stretch Challenges!
 #clear terminal between user selections √
 #***Thanks to https://www.w3resource.com/python-exercises/python-basic-exercise-99.php for python clearing help! 
-#Add a function that checks and unchecks items in list 
-#Display colored text in the terminal 
-
-#And As Always
-#Improve your code!!
+#Add a function that checks and unchecks items in list √
+#Display colored text in the terminal √ ***Thx Tanner for the help!
